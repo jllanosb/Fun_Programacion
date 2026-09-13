6c3d19e97597c7a1ffc95738e8759c3b7c11c105
Jaime Llanos <jllanosb@unc.edu.pe>
2026-09-13 13:38:22 -0500

    Encapsulamiento en C#

--- CSharp/5.Encapsulamiento/Program.cs
+++ CSharp/5.Encapsulamiento/Program.cs
@@ -1,1 +1,29 @@
 ﻿Console.WriteLine("-- Encpasulamiento --");
+
+// Restringir Acceso a campos o valores
+CtaAhorro cuenta = new CtaAhorro("Jaime", 1500.00m);
+cuenta.depositar(500);
+cuenta.mostrar_saldo();
+public class CtaAhorro
+{
+    public string titular;
+    private decimal saldo;
+    public CtaAhorro(string Titular, decimal SaldoInicial)
+    {
+        this.titular = Titular;
+        this.saldo = SaldoInicial;
+    }
+    public void depositar(decimal monto)
+    {
+        if (monto > 0)
+        {
+            saldo += monto;
+        }
+    }
+    public void mostrar_saldo()
+    {
+        Console.WriteLine($"Titular: {titular}");
+        Console.WriteLine($"Saldo: {saldo:F2}");
+    }
+}
+
