e6b844f5aac1b7191a9ed23f5ef9d43f00c6c066
Jaime Llanos <jllanosb@unc.edu.pe>
2026-09-13 08:55:38 -0500

    Variable global, no local en Python

--- Python/3.1Variables.py
+++ Python/3.1Variables.py
@@ -4,4 +4,29 @@ def saludar():
     print(f"Hola {nombre} Bienvenido")
 
 saludar()
-print(nombre)
+# print(nombre)
+
+## Variables globales
+edad=25
+
+def mostraredad():
+    global edad
+    print(f"Tu edad es: {edad}")
+
+mostraredad()
+print(type(edad))
+
+## Variable no local
+
+def principal():
+    subtotal = 0
+
+    def calcularmonto():
+        nonlocal subtotal
+        subtotal += 100
+        print(f"El nuevo valor es: {subtotal}")
+
+    calcularmonto()
+    print(f"El nuevo valor en Principal: {subtotal}")
+
+principal()
